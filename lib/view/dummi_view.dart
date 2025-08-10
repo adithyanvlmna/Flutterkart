639ee7c516617e61701ea5b70e3902ddfbb36f68
Adithyan P S <adithyanps262@gmail.com>
2025-08-10 12:46:47 +0530

    feat : dummyview internet checker commit

--- lib/view/dummi_view.dart
+++ lib/view/dummi_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterkart/core/app_theme/app_color.dart';
+import 'package:flutterkart/core/utils/internet_checker.dart';
 import 'package:flutterkart/view/auth/login_view.dart';
 import 'package:flutterkart/widgets/common_text_button.dart';
 import 'package:lottie/lottie.dart';
@@ -10,21 +11,23 @@ class DummiView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.whiteColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Lottie.asset("assets/lottie/no_data.json"),
-          CommonTextButton(
-            onPressed: () async {
-              final pref = await SharedPreferences.getInstance();
-           await  pref.clear();
-          Navigator.pushReplacementNamed(context, LoginView.routeName);
-            },
-            text: "Log Out ⬅",
-          )
-        ],
+    return ConnectivityWrapperWidget(
+      child: Scaffold(
+        backgroundColor: AppColor.whiteColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset("assets/lottie/no_data.json"),
+            CommonTextButton(
+              onPressed: () async {
+                final pref = await SharedPreferences.getInstance();
+             await  pref.clear();
+            Navigator.pushReplacementNamed(context, LoginView.routeName);
+              },
+              text: "Log Out ⬅",
+            )
+          ],
+        ),
       ),
     );
   }
